cf78d0bba76fd90c74204cee90d8dcae3e92c744
codesyariah122 <pujiermanto@gmail.com>
2024-04-17 14:29:34 +0700

    update ae

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -93,19 +93,19 @@ pwa: {
     background_color: "#000",
     icons: [
     {
-      src: "/img/icons/android-icon-48x48.png",
+      src: "/img/icons/android-launchericon-48-48.png",
       sizes: "48x48",
       type: "image/png",
       purpose: "maskable any",
     },
     {
-      src: "/img/icons/android-icon-72x72.png",
+      src: "/img/icons/android-launchericon-72-72.png",
       sizes: "72x72",
       type: "image/png",
       purpose: "maskable any",
     },
     {
-      src: "/img/icons/android-icon-96x96.png",
+      src: "/img/icons/android-launchericon-96-96.png",
       sizes: "96x96",
       type: "image/png",
       purpose: "maskable any",
@@ -117,19 +117,19 @@ pwa: {
       purpose: "maskable any",
     },
     {
-      src: "/img/icons/apple-icon-152x152.png",
+      src: "/img/icons/152.png",
       sizes: "152x152",
       type: "image/png",
       purpose: "maskable any",
     },
     {
-      src: "/img/icons/android-icon-192x192.png",
+      src: "/img/icons/android-launchericon-192-192.png",
       sizes: "192x192",
       type: "image/png",
       purpose: "maskable any",
     },
     {
-      src: "/img/icons/android-chrome-maskable-512x512.png",
+      src: "/img/icons/android-launchericon-512-512.png",
       sizes: "512x512",
       type: "image/png",
       purpose: "maskable any",
