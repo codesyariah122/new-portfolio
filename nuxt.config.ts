d6f77eb163e00045bde522a52cb374a675e10f47
codesyariah122 <pujiermanto@gmail.com>
2025-03-17 01:27:25 +0700

    add/yarnrc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    externals: {
+      inline: ['unenv']
+    }
+  },
   ssr: true,
   target: 'static',
   runtimeConfig: {
